9067efa4a0ed79cdf8e0307441b26735d08d7590
Facundo <facurtprn@gmail.com>
2023-05-23 11:20:45 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const container = document.querySelector("#tienda");
+
+
+
+
+fetch("../tienda.json")
+.then((response)=> response.json())
+.then((producto)=> {
+    producto.forEach((producto)=>{
+        let card = document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML = `
+        <img src=${producto.image} alt="" class="product-image">
+            <div class="detail-box">
+                <h2 class="card-title">${producto.title}</h2>
+                <h2 class="size">${producto.size}</h2>
+                <span class="price">${producto.price}</span>
+                <a href="https://api.whatsapp.com/send?l=es&phone=+5492615898719&text=¡Hola%20Cinthia!%20Quisiera%20encargarte%20un%20amigurumi" target="blank"><i class='bx bxl-whatsapp add-cart'></i></a>
+        `;
+
+        container.appendChild(card);
+    })
+})
